f3609bad3b405b2b9059515452a846001e2d0b22
Alex J <jones6211@gmail.com>
2018-11-03 22:42:41 -0500

    updated app files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,120 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+const TodoForm = ({addTodo}) => {
+  // input tracker
+  let input;
+
+
+  return (
+    <div className="container">
+      
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addTodo(input.value);
+        input.value = '';
+      }}>
+      <div className="input-group">
+      <input 
+      className="form-control col-sm-12 " 
+      ref={node => {
+        input = node;
+      }} 
+      placeholder='Add task here...'
+      />
+      <button className="input-group-append btn-primary shadow-none" 
+      ><i className="fa fa-plus  text-white  "></i></button>
       </div>
-    );
-  }
+      <br />
+      </form>
+    </div>
+
+  );
+};
+
+const Todo = ({todo, remove}) => {
+  // each todo
+  
+  return (<li id={todo.id} className="list-group-item" >
+      {todo.text} 
+      <span className='fa fa-times removeBtn text-info' onClick={() => {(remove(todo.id))}}></span>
+    </li>);
+    
 }
+
+const TodoList = ({todos, remove}) => {
+  //map through the todos
+  const todoNode = todos.map((todo) => {
+    return (<Todo todo={todo} key={todo.id} remove={remove}/>)
+  });
+  return (<ul className="list-group">{todoNode}</ul>)
+}
+
+
+const Title = ({todoCount}) => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <p>number of tasks ({todoCount})</p>
+    </div>
+  );
+}
+
+
+window.id = 0;
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { 
+      txt: 0,
+      data: []
+    }
+    
+  }
+  
+  // todo handler
+  addTodo(val) {
+    if(val !== '') {
+      // assemble data
+      const todo = {text: val, id: window.id++}
+      // update data
+      this.state.data.push(todo);
+      // update state
+      this.setState({
+        data: this.state.data
+      });
+      
+    }
+  }
+  
+  // handle remove
+  handleRemove(id){
+    // filter all todos except the one to be removed
+    // eslint-disable-next-line
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) return todo;
+    });
+    // update state with filter
+    this.setState({
+      data: remainder
+    });
+  }
+  
+  render() {
+    
+    return (
+        <div> 
+            <Title todoCount={this.state.data.length}/>
+            <TodoForm addTodo={this.addTodo.bind(this)}/>
+            <TodoList
+              todos={this.state.data}
+              remove={this.handleRemove.bind(this)}
+              />
+              
+        </div>
+  )}
+}
+
 
 export default App;
